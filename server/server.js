b7eed41e38d2415332ead79604e8020bbe19fd9b
Aryan Pandey <55708699+apandey00@users.noreply.github.com>
2023-04-28 09:50:20 +0000

    stable

--- server/server.js
+++ server/server.js
@@ -1,88 +1,23 @@
-﻿#!/usr/bin/env node
+const express = require('express')
+const sampleData = require('./sampleData')
+const app = express()
+const port = "3001";
 
-/**
- * Module dependencies.
- */
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const debug = require('debug')('react-express-app:server')
-const http = require('http')
-const app = require('./app')
+// MasterDetail Page Endpoint
+app.get('/api/masterdetail', (req, res) => {
+  res.json(sampleData.textAssets)
+})
 
-const CONSTANTS = require('./constants')
+// Grid Page Endpoint
+app.get('/api/grid', (req, res) => {
+  res.json(sampleData.textAssets)
+})
 
-/**
- * Get port from environment and store in Express.
- */
 
-const port = normalizePort(CONSTANTS.PORT)
-app.set('port', port)
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server 'error' event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(`${bind} requires elevated privileges`)
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      console.error(`${bind} is already in use`)
-      process.exit(1)
-      break
-    default:
-      throw error
-  }
-}
-
-/**
- * Event listener for HTTP server 'listening' event.
- */
-
-function onListening() {
-  const addr = server.address()
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-  debug(`Listening on ${bind}`)
-}
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
